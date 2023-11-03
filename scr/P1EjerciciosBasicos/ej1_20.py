b75c4bd16d6d15581a27541fabe3730ddf320137
franconil <franconil04@gmail.com>
2023-11-03 03:29:10 +0100

    asdasda

--- scr/P1EjerciciosBasicos/ej1_20.py
+++ scr/P1EjerciciosBasicos/ej1_20.py
@@ -1,6 +1,8 @@
-
 def main():
-    numerocomp = print(""+)
-    return
+    print(" Recuerda que el formato es +XX-XXXXXXXXX-XX")
+    prefijo = input("Dime el numero de telefono: ") 
+    numero = prefijo.split("-")
+    print("El numero ingresado es: "+numero[1])
+    
 if __name__ == "__main__":
     main()
