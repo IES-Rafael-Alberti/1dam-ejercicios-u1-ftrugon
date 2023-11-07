430f95f5ec7ff73bef872779aace9633a402bad1
ftrugon <fjtrujillo@iesrafaelalberti.es>
2023-11-07 19:27:50 +0100

    p8 finis

--- scr/p1.8/ej1_34.py
+++ scr/p1.8/ej1_34.py
@@ -2,13 +2,13 @@ def main():
     serie = int(input("De cuantos numeros va a ser la serie: "))
     num = 3
     cont = 1
-    linea = str(num) + " "
+    linea = str(num)    
     while cont <= serie:
         cont += 1
         num += 3
-        linea += str(num) + " "
-        #if cont == serie:
-        #   linea += str(num)
-    print(linea)
+        linea += " " + str(num)
+        if cont == serie:
+            print(linea)
+
 if __name__ == "__main__":
     main()
